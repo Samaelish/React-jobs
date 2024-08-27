ece1807b60664ee1b3d86d96b8d488add1cde9d2
Samaelish <semnd007@gmail.com>
2024-08-27 20:39:37 +0300

    added default props values to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-const Hero = ({ title, subtitle }) => {
+const Hero = ({ title = 'Become a React Dev', subtitle = 'Find the React job that fits your skill set' }) => {
   return (
     <section className='bg-indigo-700 py-20 mb-4'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
@@ -10,5 +10,4 @@ const Hero = ({ title, subtitle }) => {
     </section>
   )
 }
-
 export default Hero
