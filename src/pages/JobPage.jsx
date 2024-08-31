7a915d4204ec7586b72de5ce97c25330686edd90
Samaelish <semnd007@gmail.com>
2024-08-31 18:15:56 +0300

    added onDeleteClick

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,23 @@
-import { useParams, useLoaderData, Link } from 'react-router-dom'
+import { useParams, useLoaderData, Link, useNavigate } from 'react-router-dom'
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa'
+import { toast } from 'react-toastify'
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
   const { id } = useParams()
   const job = useLoaderData()
+
+  const navigate = useNavigate()
+
+  const onDeleteClick = jobId => {
+    const confirm = window.confirm('Are you sure you want to delete this listing?')
+
+    if (!confirm) return
+    deleteJob(jobId)
+
+    toast.success('job deleted successfully')
+
+    navigate('/jobs')
+  }
 
   return (
     <>
@@ -52,7 +66,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'>
+                <button
+                  className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
+                  onClick={() => onDeleteClick(job.id)}
+                >
                   Delete Job
                 </button>
               </div>
